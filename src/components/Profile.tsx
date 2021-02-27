1a1ecde7c3f6c9bb89b7aaa317a56f0c8786734b
Felipe de Senna <lipe.senna@hotmail.com>
2021-02-27 18:07:36 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/felipedesenna.png" alt="Felipe de Senna" />
+      <div>
+        <strong>Felipe de Senna</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
